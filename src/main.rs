a6ff6e4a3fd1641b622c78c0707b8fc9daff1f49
Empa <emanuel@empa.xyz>
2021-07-31 11:11:04 +0200

    Game is now more or less functional.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use ncurses::*;
+use rand::prelude::random;
+
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -6,15 +8,18 @@ fn main() {
     let w = initscr();
     timeout(0);
     keypad(w, true);
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    let (mut width, mut height): (i32, i32) = (0, 0);
+    getmaxyx(w, &mut height, &mut width);
 
-    let mut board = Board::new(20, 20);
+    let mut board = Board::new((width/2 - 2) as usize, (height - 2) as usize);
 
     loop {
         let tr = tick(&mut board);
         if tr == TickResult::EXIT { break; }
         render(&board);
 
-        sleep(Duration::from_millis(300));
+        sleep(Duration::from_millis(150));
     }
 
     endwin();
@@ -23,6 +28,8 @@ fn main() {
 const GFX_BLANK: &str = "  ";
 const GFX_HEAD:  &str = "@@";
 const GFX_TAIL:  &str = "<>";
+const GFX_WALL:  &str = "::";
+const GFX_FRUIT: &str = "()";
 
 #[derive(PartialEq, Eq)]
 enum Direction {
@@ -77,6 +84,16 @@ impl Head {
         self.x += dx;
         self.y += dy;
     }
+
+    fn contains_point(&self, x: i32, y: i32) -> bool {
+        if self.x == x && self.y == y { return true; }
+
+        if let Some(tail) = &self.tail {
+            return tail.contains_point(x, y);
+        }
+
+        false
+    }
 }
 
 impl Growable for Head {
@@ -114,6 +131,16 @@ impl Tail {
         self.x = x;
         self.y = y;
     }
+
+    fn contains_point(&self, x: i32, y: i32) -> bool {
+        if self.x == x && self.y == y { return true; }
+
+        if let Some(tail) = &self.tail {
+            return tail.contains_point(x, y);
+        }
+
+        false
+    }
 }
 
 impl Growable for Tail {
@@ -140,11 +167,42 @@ struct Board {
     height: usize,
 
     snake: Head,
+    fruit: Option<Fruit>,
 }
 
 impl Board {
     fn new(width: usize, height: usize) -> Board {
-        Board { width, height, snake: Head { x: 0, y: 0, tail: None, dir: Direction::NONE } }
+        let mut b = Board { width, height, snake: Head { x: 0, y: 0, tail: None, dir: Direction::NONE }, fruit: None };
+        b.spawn_fruit();
+
+        b
+    }
+
+    fn contains_point(&self, x: i32, y: i32) -> bool {
+        0 <= x && x < self.width as i32 && 0 <= y && y < self.height as i32
+    }
+
+    fn spawn_fruit(&mut self) {
+        loop {
+            let fx = (random::<f32>() * self.width as f32) as usize;
+            let fy = (random::<f32>() * self.height as f32) as usize;
+
+            if !self.snake.contains_point(fx as i32, fy as i32) {
+                self.fruit = Some(Fruit { x: fx, y: fy });
+                break;
+            }
+        }
+    }
+}
+
+struct Fruit {
+    x: usize,
+    y: usize,
+}
+
+impl Drawable for Fruit {
+    fn draw(&self, screen: &mut Vec<Vec<&str>>) {
+        screen[self.y as usize][self.x as usize] = GFX_FRUIT;
     }
 }
 
@@ -152,7 +210,6 @@ fn tick(board: &mut Board) -> TickResult {
     loop {
         let c = getch();
         if c == -1 { break };
-        println!("{}", c);
 
         if c == ('q' as i32) { return TickResult::EXIT };
 
@@ -171,6 +228,19 @@ fn tick(board: &mut Board) -> TickResult {
 
     board.snake.dmove();
 
+    // If outside board
+    if !board.contains_point(board.snake.x, board.snake.y) {
+        return TickResult::EXIT;
+    }
+
+    // If eat fruit
+    if let Some(fruit) = &board.fruit {
+        if board.snake.contains_point(fruit.x as i32, fruit.y as i32) {
+            board.spawn_fruit();
+            board.snake.grow();
+        }
+    }
+
     TickResult::CONTINUE
 }
 
@@ -179,12 +249,18 @@ fn render(board: &Board) {
 
     board.snake.draw(&mut screen);
 
+    if let Some(fruit) = &board.fruit {
+        fruit.draw(&mut screen);
+    }
+
     clear();
 
+    addstr((vec![GFX_WALL; board.width + 2].join("")).as_ref());
     for line in screen.iter_mut() {
-        let l = line.join("") + "\n";
+        let l = String::from(GFX_WALL) + &line.join("") + GFX_WALL;
         addstr(l.as_ref());
     }
+    addstr((vec![GFX_WALL; board.width + 2].join("")).as_ref());
 
     refresh();
 }
